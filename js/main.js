7c64dfa2b8ca25979ccfb486563bc3bec8c5f05b
Volohatyj <svolohatyj@gmail.com>
2025-03-13 07:46:20 +0200

    Lesson 18. Console app

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-"use strict"
+"use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?: ', '');
+
+let personalMovieDB = new Object();
+personalMovieDB['count'] = numberOfFilms;
+personalMovieDB['movies'] = {};
+personalMovieDB['actors'] = {};
+personalMovieDB['genres'] = [];
+personalMovieDB['privat'] = false;
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?: ', '');
+const lastMovieEstimation = +prompt('На сколько оцените его?: ', '');
+
+personalMovieDB.movies[lastMovie] = +lastMovieEstimation;
+
+console.log(personalMovieDB);
