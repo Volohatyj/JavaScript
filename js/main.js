6bedba4b8bf5e90b4fc341a1a5704aa557dcd2a7
Volohatyj <svolohatyj@gmail.com>
2025-03-16 12:48:42 +0200

    JS Practice,part 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-"use strict";
+'use strict'
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?: ', '');
-
-let personalMovieDB = new Object();
-personalMovieDB['count'] = numberOfFilms;
-personalMovieDB['movies'] = {};
-personalMovieDB['actors'] = {};
-personalMovieDB['genres'] = [];
-personalMovieDB['privat'] = false;
-
-let lastMovie = prompt('Один из последних просмотренных фильмов?: ', '');
-const lastMovieEstimation = +prompt('На сколько оцените его?: ', '');
-
-personalMovieDB.movies[lastMovie] = +lastMovieEstimation;
-
-console.log(personalMovieDB);
+for (let i = 0; i < lines; i++) {
+   result = '';
+   for (let j = lines - i; j > 1; j--) {
+      result += ' ';
+   }
+   // console.log(result);
+   for (let j = 0; j < i + 1; j++) {
+      if (j == 0) {
+         result += '*';
+      } else {
+         result += '**';
+      }
+   }
+   console.log(result);
+}
--- js/script.js
+++ js/script.js
@@ -23,3 +23,18 @@
 
 'use strict';
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = new Object();
+
+personalMovieDB['count'] = numberOfFilms;
+personalMovieDB['movies'] = {};
+personalMovieDB['actors'] = {};
+personalMovieDB['genres'] = [];
+personalMovieDB['privat'] = false;
+
+const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+const lastMovieEstimation = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[lastMovie] = lastMovieEstimation;
+
+console.log(personalMovieDB);
