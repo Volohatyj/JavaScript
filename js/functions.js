ec7a67e6a347d97f75422753f0ae5f053d475955
Volohatyj <svolohatyj@gmail.com>
2025-03-20 09:14:37 +0200

    2025-03-20_Practise. Functions. Advanced tasks

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,62 @@
 'use strict'
-function sayHello(name) {
-   return `Привет, ${name}!`
-};
-
-console.log(sayHello("Anton"));
-
-function returnNeighboringNumbers(num) {
-   let data = [];
-   data.push(num - 1);
-   data.push(num);
-   data.push(num + 1);
-   return data
-}
-
-console.log(returnNeighboringNumbers(5))
-
-function getMathResult(num1, num2) {
-   result = '';
-   if (typeof(num2) != 'number' || num2 <= 0) {
-      return num1;
-   };
-   for (i=1; i < num2 + 1; i++) {
-      result += num1 * i;
-      if (i < num2) {
-         result += '---';
-      }
-      // console.log(i, num1 * i)
+// Упражнение по написанию кода 7: (*) Продвинутые задания на использование функций
+function calculateVolumeAndArea(sideLength) {
+   if (!Number.isInteger(sideLength) || typeof(sideLength) != 'number' || sideLength < 0) {
+      console.log('При вычислении произошла ошибка')
+   } else {
+      const volume = Math.pow(sideLength, 3);
+      const area = Math.pow(sideLength, 2);
+      console.log(`Объем куба: ${volume}, площадь всей поверхности: ${area}`);
    }
-   return result
 }
 
-console.log(getMathResult(5, -1));
+calculateVolumeAndArea('1');
 
-console.log(typeof(0) === 'number');
+function getCoupeNumber(num) {
+   if (num > 36 || num === 0) {
+      console.log('Таких мест в вагоне не существует');
+   } else if (!Number.isInteger(num) || num < 0 || typeof(num) != 'number') {
+      console.log('Ошибка. Проверьте правильность введенного номера места');
+   } else {
+      const cupe = Math.ceil(num / 4);
+      console.log(cupe);   
+   }
+}
+
+getCoupeNumber(0);
+
+console.log(Math.pow(3, 3));
+
+// function sayHello(name) {
+//    return `Привет, ${name}!`
+// };
+
+// console.log(sayHello("Anton"));
+
+// function returnNeighboringNumbers(num) {
+//    let data = [];
+//    data.push(num - 1);
+//    data.push(num);
+//    data.push(num + 1);
+//    return data
+// }
+
+// console.log(returnNeighboringNumbers(5))
+
+// function getMathResult(num1, num2) {
+//    result = '';
+//    if (typeof(num2) != 'number' || num2 <= 0) {
+//       return num1;
+//    };
+//    for (i=1; i < num2 + 1; i++) {
+//       result += num1 * i;
+//       if (i < num2) {
+//          result += '---';
+//       }
+//       // console.log(i, num1 * i)
+//    }
+//    return result
+// }
+
+// console.log(getMathResult(5, -1));
+
