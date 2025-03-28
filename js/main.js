9456cd8d24d7976a3510823a68db3c3808c2e344
Admin <svolohatyj@gmail.com>
2025-03-28 08:56:38 +0200

    2025-03-28 Lesson 32 Arrays

--- js/main.js
+++ js/main.js
@@ -1,32 +1,23 @@
 "use strict"
 
-const options = {
-    name: 'test',
-    height: 1280,
-    width: 1920,
-    colors: {
-        border: 'black',
-        bg: 'gray'
-    },
-    printTest: function() {
-        console.log("Test");
-    }
+const arr = [10, 2, 30, 40, 5];
+
+arr.sort(sortNum);
+
+function sortNum(a, b) {
+    return a-b;
 }
 
-const {border, bg} = options.colors;
-console.log(border);
-console.log(bg);
+console.log(arr);
+arr.pop(); // видаляє останній елемент
+arr.push(10); // додає елемент в кінець масиву
 
-options.printTest();
+arr.forEach(function(value, i, arr) {
+    console.log(`${i}: ${value} is inside the list ${arr}`);
+})
 
-console.log(Object.keys(options).length);
+console.log(arr);
 
-// for (let key1 in options) {
-//     if (typeof(options[key1]) === 'object') {
-//         for (let key2 in options[key1]) {
-//             console.log(`Key: ${key2}, value: ${options[key1][key2]}`)
-//         }
-//     } else {
-//         console.log(`Key: ${key1}, value: ${options[key1]}`)
-//     }
+// for (let value of arr) {
+//     console.log(value);
 // }
