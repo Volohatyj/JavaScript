795815ccf4fa6003119b79cae7904f7e5d8bc597
Admin <svolohatyj@gmail.com>
2025-03-28 03:25:49 +0200

    2025-03-28 Методи обєктів. Деструктуризація обєктів

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 "use strict"
- function first(lang, callback){
-    console.log(`Я вчу ${lang}`);
-    callback();
- }
 
-function second() {
-    console.log("Ура!");
+const options = {
+    name: 'test',
+    height: 1280,
+    width: 1920,
+    colors: {
+        border: 'black',
+        bg: 'gray'
+    },
+    printTest: function() {
+        console.log("Test");
+    }
 }
 
-first('JS', second);
+const {border, bg} = options.colors;
+console.log(border);
+console.log(bg);
+
+options.printTest();
+
+console.log(Object.keys(options).length);
+
+// for (let key1 in options) {
+//     if (typeof(options[key1]) === 'object') {
+//         for (let key2 in options[key1]) {
+//             console.log(`Key: ${key2}, value: ${options[key1][key2]}`)
+//         }
+//     } else {
+//         console.log(`Key: ${key1}, value: ${options[key1]}`)
+//     }
+// }
