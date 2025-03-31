a86a56fff84e66f4a772bfe66a6ff4630c38d8e7
Volohatyj <svolohatyj@gmail.com>
2025-03-31 09:42:43 +0300

    2025-03-31 OOP - theory

--- js/main.js
+++ js/main.js
@@ -1,36 +1,18 @@
 "use strict"
 
-const someString = 'This is some strange string';
-
-function reverse(str) {
-    if (typeof(str) == 'string') {
-        return(str.split("").reverse().join(""))
-    } else {
-        return 'Ошибка!'
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        return "hello"
     }
-}
+};
 
+// const rhyan = {
+//         health: 100
+//     }
+    
+// Object.setPrototypeOf(rhyan, soldier);
+const rhyan = Object.create(soldier);
 
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-// const baseCurrencies = [];
-// const additionalCurrencies = [];
-
-
-function availableCurr(arr, missingCurr) {
-    let available = 'Доступные валюты:\n';
-    if (arr.length == 0) {
-        return 'Нет доступных валют'
-    };
-    arr.forEach(element => {
-        if (element != missingCurr) {
-            available += `${element}\n`;
-        }
-    });
-    return available;
-}
-
-console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'RUB'));
-
-
-console.log(reverse(someString));
+console.log(rhyan.sayHello());
