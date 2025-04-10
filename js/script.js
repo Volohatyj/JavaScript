6918016d794e093921b8060d1fcc65e6c4d167cf
Volohatyj <svolohatyj@gmail.com>
2025-04-10 09:21:56 +0300

    2025-04-10 45. Навигация по DOM - элементам, data-атрибуты, преимущество for/of FINISHED

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 'use strict';
 
-console.log('document.body');
+// console.log('document.body');
 // console.log(document.body);
-console.log(document.body.childNodes);
+// console.log(document.body.childNodes);
+// console.log(document.body.lastChild);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
 // console.log(document.documentElement);
+
+// console.log(document.querySelector('#current'));
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+
+// console.log(document.querySelector('[data-current="3"]'));
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').nextSibling.nextSibling);
+
+const childNodes = document.body.childNodes;
+for (let node of childNodes) {
+    if (node.nodeName != '#text') {
+        console.log(node);
+    };
+};
