11c46ec9a8a81ecb1a8e083c1bae7751292477a8
Admin <svolohatyj@gmail.com>
2025-04-14 08:36:27 +0300

    2025-04-14 Recursion IV

--- js/script.js
+++ js/script.js
@@ -27,5 +27,48 @@ function powRecursion(x, n) {
     }
 };
 
-console.log(`Math.pow: ${Math.pow(x, n)}, powRecursion: ${powRecursion(x, n)}`);
-console.log(`Math.pow: ${Math.pow(2, -2)}`);
+// console.log(`Math.pow: ${Math.pow(x, n)}, powRecursion: ${powRecursion(x, n)}`);
+// console.log(`Math.pow: ${Math.pow(2, -2)}`);
+
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
+
+function getTotalProgressByIteration(data) {
+    let total = 0,
+    students = 0;
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+            
+            for (let i=0; i < course.length; i++) {
+                total += course[i].progress;
+            };
+        } else {
+            
+        };
+    };
+    return total / students;
+};
+
+console.log(getTotalProgressByIteration(students));
