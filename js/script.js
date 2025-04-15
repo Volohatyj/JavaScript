e927d731452572444c3f026a75d475427f07bcda
Volohatyj <svolohatyj@gmail.com>
2025-04-15 08:30:47 +0300

    2025-04-15 Упражнение по написанию кода 16: (*) Задача на рекурсию

--- js/script.js
+++ js/script.js
@@ -1,84 +1,18 @@
 'use strict';
  
-function powIterated(x, n) {
-    let result = 1;
-    if (n === 0) {
-        return 1;
+function factorial(n) {
+    let result = 1
+    if (n <= 0) {
+        return result;
+    } else if (!Number.isInteger(n)) {
+        return 'Nan';
+    }; 
+    if (n == 1) {
+        return result;
     } else {
-        for (let i = 0; i < n; i++) {
-            result *= x;
-            console.log(`i: ${i}, result: ${result}`);
-        };        
-        return result
-    }
-};
-
-// powIterated(2, 5);
-
-const x = 2,
-    n = 8;
-
-
-function powRecursion(x, n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return x * powRecursion (x, n - 1);
-    }
-};
-
-// console.log(`Math.pow: ${Math.pow(x, n)}, powRecursion: ${powRecursion(x, n)}`);
-// console.log(`Math.pow: ${Math.pow(2, -2)}`);
-
-let students = {
-    js: [{
-        name: 'John',
-        progress: 100
-    }, {
-        name: 'Ivan',
-        progress: 60
-    }],
-    html: {
-        basic: [{
-            name: 'Peter',
-            progress: 20
-        }, {
-            name: 'Ann',
-            progress: 18
-        }],
-
-        pro: [{
-            name: 'Sam',
-            progress: 10
-        }]
-    }
-};
-
-function getTotalProgressByIteration(data) {
-    let total = 0,
-    students = 0;
-    for (let course of Object.values(data)) {
-        if (Array.isArray(course)) {
-            students += course.length;
-            
-            for (let i=0; i < course.length; i++) {
-                total += course[i].progress;
-            };
-        } else {
-            for (let subCourse of Object.values(course)){
-                students += subCourse.length;
-
-                for (let i=0; i < subCourse.length; i++) {
-                    total += subCourse[i].progress;
-                };
-            };
-        };
+        result *= n*factorial(n-1); 
     };
-    return total / students;
+    return result;
 };
 
-console.log(getTotalProgressByIteration(students));
-
-function getTotalProgressByRecursion(data) {
-    
-};
+console.log(factorial(0.1));
