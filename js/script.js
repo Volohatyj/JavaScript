5667a837151947f6cb20f20fc507a834afbe064a
Volohatyj <svolohatyj@gmail.com>
2025-04-18 11:01:19 +0300

    2025-04-18_47_Практика_ИспользуемСсобытияНаСтраницеПроекта_I

--- js/script.js
+++ js/script.js
@@ -15,38 +15,99 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-const element = document.querySelector('.promo__adv'),
-    poster = document.querySelector('.promo__bg'), 
-    genre = poster.querySelector('.promo__genre'),
-    interactiveList = document.querySelector('.promo__interactive-list');
 
-if (element) {
-  element.remove(); // Видаляємо елемент з DOM
-}
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelector('.promo__adv'),
+        poster = document.querySelector('.promo__bg'), 
+        genre = poster.querySelector('.promo__genre'),
+        interactiveList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'), // форма, в якої є клас .add
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type = "checkbox"]');
 
-// genre.innerHTML = 'ДРАМА';
-genre.textContent = 'ДРАМА';
-console.dir(poster);
-if (poster) {
-    poster.style.backgroundImage = 'url("img/bg.jpg")'; // Встановлюємо нове фонове зображення
-  }
+    // Обробщик подій для форми, щоб відстежити відправку форми
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault(); // відміна стандартної поведінки браузера: при натисканні кнопки "Відправити" сторінка не будitе перезавантажуватись
+        
+        // Що саме ввів користувач і чи відмітив чекбокс?
+        let newFilm = addInput.value; // Отримуємо те, що ввів користувач
+        const favorite = checkbox.checked; // Статус чекбокса
 
-interactiveList.innerHTML = '';
-movieDB.movies.sort();
-movieDB.movies.forEach((film, i) => {
-    interactiveList.innerHTML += `
-         <li class="promo__interactive-item">${i+1}. ${film}
-            <div class="delete"></div>
-        </li>
-    `;
+        if (newFilm) { // Перевірка, щоб ігнорувати порожній ввід даних в форму
+            if (newFilm.length > 21) { // Обрізаємо назву фільма до 21 знака
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            };           
+
+            // Створення нового фільму та додавання його в список переглянутих фільмів
+            // Додавання фільму в базу даних і відсортуємо
+            movieDB.movies.push(newFilm); // Додавання фільму в базу даних (в кінець БД)
+            sortArray(movieDB.movies); // Сортуємо список фільмів
+    
+            //Створення нових елементів на сторінці
+            createMovieList(movieDB.movies, interactiveList)   
+        };
+
+        
+        // Очищаємо форму
+        // addForm.reset();
+        event.target.reset();
+    });
+    
+/*
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove(); // Видаляємо елемент з DOM (реклама)
+        });
+    };
+    */
+   const deleteAdv = (arr) => {
+    if (adv) {
+        adv.remove(); // Видаляємо елемент з DOM (реклама)
+      }
+   };
+    
+    
+    const makeChanges = () => {
+        // genre.innerHTML = 'ДРАМА';
+        genre.textContent = 'ДРАМА';
+        if (poster) {
+            poster.style.backgroundImage = 'url("img/bg.jpg")'; // Встановлюємо нове фонове зображення
+        };        
+    };
+
+    
+    const sortArray = (arr) => {
+        arr.sort();
+    };
+      
+    function createMovieList(films, parent) {
+        // Створення нових елементів на сторінці (список фільмім з БД) 
+        parent.innerHTML = '';
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                 <li class="promo__interactive-item">${i+1}. ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+    };
+
+
+    deleteAdv(adv);
+    makeChanges();
+    sortArray(movieDB.movies);
+    createMovieList(movieDB.movies, interactiveList);
+
 });
