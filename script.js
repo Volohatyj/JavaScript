b1b4a05138ff468c47b27d86f01bbf9d4a81a8d3
Volohatyj <svolohatyj@gmail.com>
2025-05-06 09:15:11 +0300

    62_ClassList

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
 'use strict';
 
-const boxesQuery = document.querySelectorAll('.box');
-const boxesGet = document.getElementsByClassName('box');
+const btns = document.querySelectorAll('button');
 
-/*
-boxesQuery[0].remove();
-boxesGet[0].remove();
-
-for (let i = 0; i < 5; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    document.body.append(div);
-    // boxesGet[boxesGet.length] = div; // Не правильний підхід. Видає помилку. Ми не можемо на пряму змінювати HTMLCollection
-}
-
-console.log('boxesQuery:');
-console.log(boxesQuery);
-console.log('boxesGet:');
-console.log(boxesGet);
-// console.log(document.body.children);
-
-// Створення масиву з масиво-подібного об'єкту
-console.log('Array.from(boxesGet):');
-console.log(Array.from(boxesGet)); // => отримуємо звичайний масив, він статичний і не слідкує за змінами в DOM-дереві
-
-const boxesQuery2 = document.querySelectorAll('.box');
-console.log(boxesQuery2);
-*/
-
-// Іноді серед всіх елементів портібно знайти тільки той що підходить за певними параметрами
-boxesQuery.forEach(box => {
-    if (box.matches('.this')) {
-        console.log("This One!");
-        console.log(box.outerHTML);
-    }
-});
-
-// По всьому ланцюжку наверх шукає зазначений селектор ("wrapper") і виводить всі його дочірні елементи. Корисно, коли є складна розгалужена верстка
-console.log(boxesQuery[0].closest('.wrapper'));
+console.log(btns[0].classList.length); // кількість класів в елементу
+console.log(btns[0].classList.item(0)); // повертає клас blue який знаходиться під певним індексом
+console.log(btns[0].classList.item(1)); // повертає клас some який знаходиться під певним індексом
+console.log(btns[0].classList.add('red')); // додає клас red
+console.log(btns[0].classList.add('blue')); // видаляє клас blue
+console.log(btns[0].classList.toggle('blue')); // 
